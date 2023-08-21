e7b7eaeafdfd3a74b890edbd7adf44cb996dbbe6
Farhankhan18 <125142393+Farhankhan18@users.noreply.github.com>
2023-08-21 11:25:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("contact-toggle").addEventListener("click", function() {
+  var contactList = document.getElementById("contact-list");
+  contactList.classList.toggle("hidden");
+});
+document.getElementById("projects-toggle").addEventListner("click",function(){
+  var projectList = document.getElementById("project-list");
+  projectList.projectList.toggle("hidden");
+};
